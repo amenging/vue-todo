f776d63c95d4bc2afbbe5615927bf0e496c2c6cb
ameng <1508235075@qq.com>
2018-08-17 15:56:11 +0800

    接上一步修改

--- server/db.php
+++ server/db.php
@@ -3,6 +3,10 @@
 	$dbuser = 'root';
 	$dbpass = 'root';
 
+	try {
+		include 'remote.php';
+	} catch (Exception $e) {}
+
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 	if(!$conn) {
 		die('连接错误' . mysqli_error($conn));
