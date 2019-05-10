9891a4cd9364b91522e8fb4fea4233e8d00f865a
amenging <1508235075@qq.com>
2019-05-10 12:07:05 +0800

    cdn

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [
+    link: [ 
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
@@ -78,6 +78,8 @@ module.exports = {
     extend(config, ctx) {
 
     },
+
+    publicPath: 'https://d1krgmedyqpj9s.cloudfront.net'
   },
 
 }
